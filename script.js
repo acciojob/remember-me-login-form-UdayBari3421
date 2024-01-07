3f9f34c9f770416888c849279242a3285e8e4693
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-01-07 11:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const checkbox = document.querySelector("#checkbox");
+const form = document.querySelector("form");
+const submit = document.querySelector("#submit");
+
+form.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	let btn = document.createElement("input");
+	if (checkbox.checked && username.value !== "") {
+		if (localStorage.getItem("username") === username.value) {
+				let user = localStorage.getItem("username");
+				let pass = localStorage.getItem("password");
+
+			username.addEventListener("input",()=>{
+				btn.setAttribute("value", "Login as exiting user");
+				btn.setAttribute("type", "submit");
+				btn.setAttribute("id", "btn");
+				form.appendChild(btn);
+				password.value = pass.value;
+				checkbox.checked = true;
+				submit.style.display = "none";
+			});
+			username.value = user.value;
+			alert(`Logged in as ${user}`);
+		}
+		else{
+			localStorage.setItem("username",username.value);
+			localStorage.setItem("password",password.value);
+			alert(`Logged in as ${username.value}`);
+		}
+		form.reset();
+		// form.removeChild(btn);
+		btn.style.display = "block";
+	}
+	else{
+		localStorage.getItem("username");
+		localStorage.getItem("password");
+		alert("Enter Details First");
+	}
+	btn.style.display = "block";
+	// submit.style.display = "none";
+});
