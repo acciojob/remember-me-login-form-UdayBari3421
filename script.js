4bca09979222c57daf95744f8a7e5818c001d6ff
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-01-08 22:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const user = document.querySelector("#username");
 const pass = document.querySelector("#passowrd");
 const checkbox = document.querySelector("#checkbox");
 const submit = document.querySelector("#submit");
-
+let obj;
 form.addEventListener("submit",(event)=>{
-	
+	obj = {
+		usernam: form.username.value,
+		passwor: form.password.value,
+	};
 	event.preventDefault();
 	
-	if(checkbox.checked && user.value !== ""){
+	if(checkbox.checked){
 		let username = localStorage.getItem("username");
 		let password = localStorage.getItem("password");
 		
-		if (username == user.value) {
+		if (username == obj.usernam) {
 			localStorage.removeItem("username");
 			localStorage.removeItem("password");
-			alert(`Logged in as ${user.value}`);
+			alert(`Logged in as ${obj.usernam}`);
 		} 
-		localStorage.setItem("username",user.value);
-		localStorage.setItem("password",pass.value);
+		localStorage.setItem("username",obj.usernam);
+		localStorage.setItem("password",obj.passwor);
+		console.log(obj.usernam);
+		console.log(obj.passwor);
 		
 	} else{
 		alert("Logged in as username");
@@ -26,10 +31,11 @@ form.addEventListener("submit",(event)=>{
 	form.reset();
 });
 
-// user.addEventListener("input",()=>{
-// 	if(user.value == localStorage.getItem("username")){
-// 		existing.style.display = "block";
-// 	} else{
-// 		existing.style.display = "none";
-// 	}
-// });
+user.addEventListener("input",()=>{
+	
+	if(obj.usernam == localStorage.getItem("username")){
+		existing.style.display = "block";
+	} else{
+		existing.style.display = "none";
+	}
+});
